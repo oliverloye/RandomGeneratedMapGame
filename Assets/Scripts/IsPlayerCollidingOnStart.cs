5d3bda67200f0bfcb412144082b10bbec01bf06b
Oliver <oliverloye@me.com>
2019-03-25 14:41:56 +0100

    diablo2

---
+++ Assets/Scripts/IsPlayerCollidingOnStart.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IsPlayerCollidingOnStart : MonoBehaviour
+{
+
+
+    public bool imColliding = false;
+    public GameObject player;
+
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(imColliding == false)
+        {
+            Instantiate(player, transform.position, Quaternion.identity);
+            Destroy(gameObject,1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (imColliding == true)
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "tile")
+        {
+            print("COLLISION");
+            imColliding = true;
+        }
+    }
+}
+
---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private Vector3 target;
+
+    private void Start()
+    {
+        target = transform.position;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
+
+
+/* 
+Rigidbody2D body;
+
+    float horiziontal;
+    float vertical;
+
+    public float runSpeed = 20.0f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horiziontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horiziontal * runSpeed, vertical * runSpeed);
+    }
+    */
